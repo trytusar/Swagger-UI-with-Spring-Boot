2cc03b63627d9bf4e1f5ac08a1066065ae16e984
Tusarkant Sahoo <trytusar@gmail.com>
2019-12-22 22:41:13 +0530

    Extracted Code

---
+++ src/main/java/com/howtodoinjava/rest/SpringBootDemoApplication.java
@@ -0,0 +1,14 @@
+//http://localhost:8080/swagger-ui.html
+
+package com.howtodoinjava.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication; 
+
+@SpringBootApplication 
+public class SpringBootDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDemoApplication.class, args);
+    }
+}
